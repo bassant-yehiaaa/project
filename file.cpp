7029590e22d59193a644ecd5a972c04fedd965a5
bassant-yehiaaa <faroukayman582@gmail.com>
2025-12-23 03:04:48 +0200

    final

--- file.cpp
+++ file.cpp
@@ -1,5 +1,16 @@
-#include  <iostream>
-using namespace std ;
-int main (){
-    cout <<"Hello in my image";
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    const char* envAppName = getenv("APP_NAME");
+
+    string appName = envAppName ? envAppName : "DefaultApp";
+
+    cout << "APP_NAME: " << appName << endl;
+
+    return 0;
 }
